89625256e70d9502ba3550230eda80ea9160457e
KERRCAM <kerrcameron@hotmail.com>
2021-12-10 00:35:18 +0000

    basicMathFunction

--- main.py
+++ main.py
@@ -14,12 +14,37 @@ def stringInput(prompt):
             continue
 
 
-def calculator():
-    print(2)
+def intInput(prompt):
+    while True:
+        try:
+            userInput = int(input(prompt))
+            return userInput
+            break
+        except Exception:
+            print("incorrect input")
+            continue
 
+
+def calculator():
+    def add(current):
+        numberToAdd = intInput("enter number to add to current: ")
+        print("...")
+        time.sleep(1)
+        return current + numberToAdd
+
+    current = int(intInput("enter starting number: "))
+    calculating = True
+    while calculating == True:
+        function = intInput("what would you like to do to current number? (enter number of choice) (1)-add number- : ")
+        if function == 1:
+            current = add(current)
+            print("new number is:")
+            print(current)
 
 def menu():
-    choice = stringInput("what would you like to use? (enter number of choice) (1)-calculator- : ")
+    choice = intInput("what would you like to use? (enter number of choice) (1)-calculator- : ")
+    print("...")
+    time.sleep(1)
     if choice == 1:
         calculator()
 
